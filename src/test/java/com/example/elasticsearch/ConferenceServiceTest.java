3e958e94428885ff5e0795397db4c576609654f8
zhuhaitao <zhuhaitao@email.com>
2020-12-05 13:45:56 +0800

    init():仅保留基础的配置以及测试用例

--- src/test/java/com/example/elasticsearch/ConferenceServiceTest.java
+++ src/test/java/com/example/elasticsearch/ConferenceServiceTest.java
@@ -1,23 +1,21 @@
 package com.example.elasticsearch;
 
-import com.example.elasticsearch.entity.Conference;
 import com.example.elasticsearch.service.ConferenceService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 
 /**
  * @author haitao zhu
  * @date 2020/11/30 9:55
  */
-public class ConferenceServiceTest  extends RootTest {
+public class ConferenceServiceTest extends RootTest {
 
   @Autowired
   private ConferenceService conferenceService;
 
   @Test
-  public void findTest(){
-    Page<Conference> page = conferenceService.getPage(1, 10);
-    page.forEach(System.out::println);
+  public void findAllTest() {
+    conferenceService.getAll().forEach(System.out::println);
   }
+
 }
