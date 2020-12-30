673faf93cadcc639e837e12d66e7a70951cd3032
zhuhaitao <zhuhaitao@email.com>
2020-12-30 16:17:41 +0800

    fix():移除多余的测试，仅保留最初的保存与查询

--- src/main/java/com/example/elasticsearch/service/ConferenceService.java
+++ src/main/java/com/example/elasticsearch/service/ConferenceService.java
@@ -1,23 +1,12 @@
 package com.example.elasticsearch.service;
 
 import com.example.elasticsearch.entity.Conference;
-import com.example.elasticsearch.entity.querydsl.QConference;
-import com.example.elasticsearch.repository.ConferencePagingAndSortingRepository;
 import com.example.elasticsearch.repository.ConferenceRepository;
-import com.example.elasticsearch.repository.ConferenceRepositoryQuerydslPredicate;
-import com.querydsl.core.types.Predicate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.geo.GeoPoint;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.time.ZonedDateTime;
 import java.util.Arrays;
 
 /**
@@ -31,27 +20,10 @@ public class ConferenceService {
   private ElasticsearchOperations elasticsearchOperations;
   @Autowired
   private ConferenceRepository repository;
-  @Autowired
-  private ConferencePagingAndSortingRepository pagingAndSortingRepository;
-  @Autowired
-  private ConferenceRepositoryQuerydslPredicate querydslPredicate;
-  @PersistenceContext
-  private EntityManager em;
 
-  private final Logger log = LoggerFactory.getLogger(ConferenceService.class);
 
-  public void querydslPredicateTest() {
-    QConference conference = QConference.conference;
-    Predicate predicate = conference.name.equalsIgnoreCase("name").and(conference.date.eq(ZonedDateTime.now()));
-    querydslPredicate.findAll(predicate);
-  }
-
-  public Page<Conference> getPage(int page, int size) {
-    Page<Conference> conferences = pagingAndSortingRepository.findAll(PageRequest.of(page, size));
-    if (conferences.isFirst()) {
-      log.info("slice 这个方法也不知道能干什么");
-    }
-    return conferences;
+  public Iterable<Conference> findAll() {
+    return repository.findAll();
   }
 
   public void insertDataSample() {
--- src/test/java/com/example/elasticsearch/ConferenceServiceTest.java
+++ src/test/java/com/example/elasticsearch/ConferenceServiceTest.java
@@ -4,22 +4,20 @@ import com.example.elasticsearch.entity.Conference;
 import com.example.elasticsearch.service.ConferenceService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-
-import java.util.function.Function;
 
 /**
  * @author haitao zhu
  * @date 2020/11/30 9:55
  */
-public class ConferenceServiceTest  extends RootTest {
+public class ConferenceServiceTest extends RootTest {
 
   @Autowired
   private ConferenceService conferenceService;
 
   @Test
-  public void findTest(){
-    Page<Conference> page = conferenceService.getPage(1, 10);
-    page.forEach(System.out::println);
+  public void findTest() {
+    Iterable<Conference> all = conferenceService.findAll();
+    all.forEach(System.out::println);
+
   }
 }
